6f80b965fee5ee0452f25d0c29037e08c470e084
AurélienARNAULT <aurelien.hand06@gmail.com>
2020-10-16 08:43:06 +0200

    Avance du main console

--- src/main/java/MainConsole.java
+++ src/main/java/MainConsole.java
@@ -1,7 +1,10 @@
+import java.io.IOException;
+import controleur.ControleurJeu;
 
 public class MainConsole {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, IOException {
+		ControleurJeu Controleur = new ControleurJeu();
 
 	}
 
