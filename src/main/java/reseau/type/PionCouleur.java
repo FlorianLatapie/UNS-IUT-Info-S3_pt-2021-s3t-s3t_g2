f53e18dd773b560256d683054b40012ab1790cad
Sebastien Aglae <sebastien.aglae@gmail.com>
2020-10-17 19:44:56 +0200

    Enumerations pour les paquets

---
+++ src/main/java/reseau/type/PionCouleur.java
@@ -0,0 +1,12 @@
+package reseau.type;
+
+public enum PionCouleur {
+    NBL, NBR, NT, NF,
+    VBL, VBR, VT, VF,
+    BBL, BBR, BT, BF,
+    RBL, RBR, RT, RF,
+    JBL, JBR, JT, JF,
+    MBL, MBR, MT, MF,
+    NUL
+}
+
---
+++ src/main/java/reseau/type/VigileEtat.java
@@ -0,0 +1,5 @@
+package reseau.type;
+
+public enum VigileEtat {
+    NE, NUL
+}
