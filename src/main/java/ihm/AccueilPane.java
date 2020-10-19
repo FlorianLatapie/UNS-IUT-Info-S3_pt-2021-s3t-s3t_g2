4a5331c09851f31d2432de4323ab93ad2a75d326
Florian LATAPIE <70631622+FlorianLatapie@users.noreply.github.com>
2020-10-19 19:18:59 +0200

    changement de taille des boutons

--- src/main/java/ihm/AccueilPane.java
+++ src/main/java/ihm/AccueilPane.java
@@ -3,6 +3,7 @@ package ihm;
 import ihm.ScreenControl;
 import ihm.DataControl.ApplicationPane;
 import javafx.application.Platform;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -10,6 +11,7 @@ import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -73,12 +75,16 @@ public class AccueilPane extends BorderPane { //ATTENTION : le type Pane est à 
 		this.setCenter(vbcentre);*/
 		
 		Label titre = new Label("ZOMBIES la blonde la brute et le truand");
-		titre.setFont(Font.font("Segoe UI", 50));
+		titre.setFont(Font.font("Segoe UI", 30));
 		
 		Button Jouer = new Button("Jouer");
+		Jouer.setPrefSize(100, 50);
 		Button Options = new Button("Options");
+		Options.setPrefSize(100, 50);
 		Button Regles = new Button("Règles");
+		Regles.setPrefSize(100, 50);
 		Button Quitter = new Button("Quitter");
+		Quitter.setPrefSize(100, 50);
 		
 		Quitter.setOnAction(
 				event -> {
@@ -97,10 +103,18 @@ public class AccueilPane extends BorderPane { //ATTENTION : le type Pane est à 
 		centreMenu.setAlignment(Pos.CENTER);
 		
 		
-		
 		HBox fond = new HBox(centreMenu);
 		fond.setAlignment(Pos.CENTER);
-		centreMenu.getChildren().addAll(titre,Jouer,Options,Regles,Quitter);
+		
+		GridPane grilleBoutons = new GridPane();
+		grilleBoutons.setAlignment(Pos.CENTER);
+		grilleBoutons.add(Jouer, 0, 0);
+		grilleBoutons.add(Options,0 ,1);
+		grilleBoutons.add(Regles, 1, 0);
+		grilleBoutons.add(Quitter, 1, 1);
+		
+		centreMenu.setMargin(titre,new Insets(0,0,100,0));
+		centreMenu.getChildren().addAll(titre,grilleBoutons);
 		
 		fond.setPrefWidth(100);
 		fond.setPrefHeight(100);
