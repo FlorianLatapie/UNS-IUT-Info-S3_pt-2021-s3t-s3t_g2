0e78e04b4f74b53b931f4e19c28cab96c0d3b69a
Florian LATAPIE <70631622+FlorianLatapie@users.noreply.github.com>
2020-10-20 19:40:10 +0200

    Version 0.1 de l'interface

--- src/main/java/ihm/AccueilPane.java
+++ src/main/java/ihm/AccueilPane.java
@@ -7,6 +7,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
@@ -18,83 +19,124 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 
-
-public class AccueilPane extends BorderPane { //ATTENTION : le type Pane est à changer par le type de votre choix.
-	
+/**
+ * The Class AccueilPane.
+ * 
+ * @author Florian
+ * @version 0.1
+ * @since 04/10/2020
+ */
+public class AccueilPane extends BorderPane { 
+	//ScreenControl permet de gérer les variables globales comme la langue ou le volume 
 	private ScreenControl sControl = null;
-	private final ApplicationPane paneName = ApplicationPane.ACCUEIL;
-	
-	private int tailleCarreCentral = 600;
-	private int HBouton = tailleCarreCentral/8;
-	private int LBouton = tailleCarreCentral/4;
-	private int marge = tailleCarreCentral/40;
-	private Insets margeBoutons = new Insets(marge,marge,marge,marge) ; 
+	private final ApplicationPane paneName = ApplicationPane.ACCUEIL; //nom du pane 
+	//définition des variable pour la suite du pane 
+	private int tailleCarreCentral = 600; //l'interface est sur un stackPane qui peut trouner avec des crans dd 90 degrés 
+	private int hBouton = 100;
+	private int lBouton = 200;
+	private int marge = tailleCarreCentral / 25;
+	private Insets margeBoutons = new Insets(marge, marge, marge, marge);
+	private Font policeBouton = Font.font("Segoe UI", tailleCarreCentral / 17);
+	private CornerRadii coin = new CornerRadii(15.0);
+	private String coinBoutons = " -fx-background-radius: 20px";
 
 	public AccueilPane(ScreenControl sc) {
 
 		sControl = sc;
-		Label titre = new Label("ZOMBIES la blonde la brute et le truand");
-		titre.setFont(Font.font("Segoe UI", tailleCarreCentral/20));
 		
-		Button Jouer = new Button("Jouer");
-		Jouer.setPrefSize(LBouton, HBouton);
-		Button Options = new Button("Options");
-		Options.setPrefSize(LBouton, HBouton);
-		Button Regles = new Button("Règles");
-		Regles.setPrefSize(LBouton, HBouton);
-		Button Quitter = new Button("Quitter");
-		Quitter.setPrefSize(LBouton, HBouton);
+		// titre 
+		Label titre1 = new Label("ZOMBIES");
+		titre1.setFont(Font.font("Segoe UI", 60));
+		titre1.setTextFill(Color.BLACK);
 		
-		Options.setOnAction(EventHandler -> sc.setPaneOnTop(ApplicationPane.OPTION));
+		Label titre2 = new Label("LA BLONDE LA BRUTE ET LE TRUAND");
+		titre2.setFont(Font.font("Segoe UI", 25));
+		titre2.setTextFill(Color.BLACK);
 		
-		Quitter.setOnAction(
-				event -> {
-					Platform.runLater(new Runnable() {
-						public void run() {
-							Platform.exit();
-						}
-					});
+		VBox titre = new VBox(titre1,titre2);
+		titre.setAlignment(Pos.CENTER);
+		titre.setBackground(new Background(new BackgroundFill(Color.RED, coin, null)));
+		
+		
+		// boutons 
+		Button bJouer = new Button("Jouer");
+		bJouer.setPrefSize(lBouton, hBouton);
+		bJouer.setMinSize(lBouton, hBouton);
+		bJouer.setFont(policeBouton);
+		bJouer.setStyle(coinBoutons);
+
+		Button bOptions = new Button("Options");
+		bOptions.setPrefSize(lBouton, hBouton);
+		bOptions.setMinSize(lBouton, hBouton);
+		bOptions.setFont(policeBouton);
+		bOptions.setStyle(coinBoutons);
+		
+		Button bRegles = new Button("Règles");
+		bRegles.setPrefSize(lBouton, hBouton);
+		bRegles.setMinSize(lBouton, hBouton);
+		bRegles.setFont(policeBouton);
+		bRegles.setStyle(coinBoutons);
+
+		Button bQuitter = new Button("Quitter");
+		bQuitter.setPrefSize(lBouton, hBouton);
+		bQuitter.setMinSize(lBouton, hBouton);
+		bQuitter.setFont(policeBouton);
+		bQuitter.setStyle(coinBoutons);
+
+		// Jouer.setOnAction(EventHandler -> sc.setPaneOnTop(ApplicationPane.JOUER));
+		bOptions.setOnAction(EventHandler -> sc.setPaneOnTop(ApplicationPane.OPTION));
+		// Regles.setOnAction(EventHandler -> sc.setPaneOnTop(ApplicationPane.Regles));
+		bQuitter.setOnAction(event -> {
+			Platform.runLater(new Runnable() {
+				public void run() {
+					Platform.exit();
 				}
-			);
+			});
+		});
+
 		
-		VBox centreMenu = new VBox(); 
-		centreMenu.setBackground(new Background(new BackgroundFill(Color.BLUE,CornerRadii.EMPTY,null)));
+		// grille contenant les 4 boutons 
+		GridPane grilleBoutons = new GridPane();
+		grilleBoutons.setAlignment(Pos.CENTER);
+		grilleBoutons.add(bJouer, 0, 0);
+		grilleBoutons.add(bOptions, 0, 1);
+		grilleBoutons.add(bRegles, 1, 0);
+		grilleBoutons.add(bQuitter, 1, 1);
+
+		grilleBoutons.setMargin(bJouer, margeBoutons);
+		grilleBoutons.setMargin(bOptions, margeBoutons);
+		grilleBoutons.setMargin(bRegles, margeBoutons);
+		grilleBoutons.setMargin(bQuitter, margeBoutons);
+		
+		// image fond
+		//ImageView imgFond = new ImageView("../existe pas.extension");
+		
+		
+		// carre central qui contient tous les éléments (bouton et titre) 
+		VBox centreMenu = new VBox();
+		//centreMenu.setBackground(new Background(new BackgroundFill(Color.LIGHTGREY,CornerRadii.EMPTY,null)));
 		centreMenu.setPrefSize(tailleCarreCentral, tailleCarreCentral);
 		centreMenu.setMaxSize(tailleCarreCentral, tailleCarreCentral);
 		centreMenu.setAlignment(Pos.CENTER);
+		centreMenu.setMargin(titre, new Insets(0, 0, 100, 0));
+		//titre.setPadding(margeBoutons);
+		centreMenu.getChildren().addAll(titre, grilleBoutons);
 		
+		// rotation de l'interface 
+		//centreMenu.setRotate(90);
 		
+		// boite du fond qui contient tout 
 		HBox fond = new HBox(centreMenu);
 		fond.setAlignment(Pos.CENTER);
-		
-		GridPane grilleBoutons = new GridPane();
-		grilleBoutons.setAlignment(Pos.CENTER);
-		grilleBoutons.add(Jouer, 0, 0);
-		grilleBoutons.add(Options,0 ,1);
-		grilleBoutons.add(Regles, 1, 0);
-		grilleBoutons.add(Quitter, 1, 1);
-		
-		grilleBoutons.setMargin(Jouer, margeBoutons);
-		grilleBoutons.setMargin(Options, margeBoutons);
-		grilleBoutons.setMargin(Regles, margeBoutons);
-		grilleBoutons.setMargin(Quitter, margeBoutons);
-		
-		
-		centreMenu.setMargin(titre,new Insets(0,0,100,0));
-		centreMenu.getChildren().addAll(titre,grilleBoutons);
-		
 		fond.setPrefWidth(100);
 		fond.setPrefHeight(100);
-		fond.setAlignment(Pos.CENTER);
-		fond.setBackground(new Background(new BackgroundFill(Color.ORANGE,CornerRadii.EMPTY,null)));
-		//root.setTop(top);
-		
+		fond.setBackground(new Background(new BackgroundFill(Color.BLACK,CornerRadii.EMPTY,null)));
+		//fond.getChildren().add(imgFond);
 		this.setCenter(fond);
-		
+
 		sControl.registerNode(paneName, this);
 		sControl.setPaneOnTop(paneName);
-		
-		
+
 	}
 
 }
