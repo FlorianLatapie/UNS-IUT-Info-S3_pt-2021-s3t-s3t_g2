36034e39d81da6410f804a51f0540d48d0ff2f98
G2-VincentCalatayud <70631929+G2-VincentCalatayud@users.noreply.github.com>
2020-10-21 17:45:36 +0200

    create pause
    creation de la scene pause

---
+++ src/main/java/ihm/src/controllers/Pause.java
@@ -0,0 +1,58 @@
+spackage controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Pause {
+
+    @FXML
+    private Button Options;
+
+    @FXML
+    private Button Regles_du_jeu;
+
+    @FXML
+    private Button Re;
+
+    @FXML
+    private Button Quitter;
+
+    @FXML
+    private Button Retour;
+
+
+    @FXML
+    private void initialize() {
+        Options.setText("Options");
+        Regles_du_jeu.setText("Regles");
+        Re.setText("recommencer");
+        Quitter.setText("Quitter");
+        Retour.setText("retour");
+
+    }
+
+    @FXML
+    private void clickedButton (ActionEvent event) throws Exception {
+        Stage stage = null;
+        Parent root = null;
+
+        if(event.getSource() == Quitter) {
+            System.exit(0);
+        }
+
+        else {
+            stage = (Stage) Options.getScene().getWindow();
+            root = FXMLLoader.load(getClass().getResource("/views/MenuView.fxml"));
+        }
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
