94d079ed7a471f1b890bf26f5893297faa4c3c26
AlexDevauchelle <70631774+AlexDevauchelle@users.noreply.github.com>
2020-10-22 12:37:39 +0200

    Create JeuTest.java

---
+++ src/test/java/jeu/JeuTest.java
@@ -0,0 +1,20 @@
+package jeu;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JeuTest {
+
+	@Test
+	void testJeu_Constructor() {
+		fail("Not yet implemented");
+	}
+
+	void testJeu_Methode1() {
+		fail("Not yet implemented");
+	}
+	void testJeu_Methode2() {
+		fail("Not yet implemented");
+	}
+}
