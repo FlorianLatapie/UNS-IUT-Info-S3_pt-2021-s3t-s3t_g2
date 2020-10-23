0a26929763b9b1d4b86f1816e6bc347381a02779
Florian LATAPIE <70631622+FlorianLatapie@users.noreply.github.com>
2020-10-23 16:23:58 +0200

    création des lieux sur le plateau

--- src/main/java/ihm/PlateauPane.java
+++ src/main/java/ihm/PlateauPane.java
@@ -5,23 +5,41 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
-public class PlateauPane extends BorderPane {
+public class PlateauPane extends StackPane {
 	private ScreenControl sControl = null;
 	private final ApplicationPane paneName = ApplicationPane.PLATEAU; // nom du pane
 	
 	private int margeP = 24;
 	private Insets margeBoutonPause = new Insets(margeP, margeP, margeP, margeP);
 	private int lhBoutonPause = 80;
-	
 	private String coinBoutons = " -fx-background-radius: 5px";
 	
+	private int margeJ = 20;
+	private Insets margeTexteJoueur = new Insets(margeJ, 10,margeJ,10);
+	private Insets insetJGauche =new Insets(0,500,0,115);
+	private Insets insetJDroit = new Insets(0,115,0,500);
+	private int tailleVBoxJoueur = 210;
+	
+	private int taillePlateau = 1080;
+	
+	private int margeL = 10;
+	private Insets margeLieu = new Insets(margeL,margeL,margeL,margeL);
+	private int tailleFont = 15;
+	private Font fontInfo = Font.font("Segoe UI", tailleFont);
+	private Font fontTitre = Font.font("Segoe UI", FontWeight.BOLD, tailleFont);
 	
 	public PlateauPane(ScreenControl sc) {
 		sControl = sc;
@@ -30,8 +48,11 @@ public class PlateauPane extends BorderPane {
 		HBox hDroite = new HBox(); 
 		HBox hBas = new HBox(); 
 		HBox hGauche = new HBox(); 
-		StackPane sCentre = new StackPane(); 
+		//StackPane sBase = new StackPane(); 
+		BorderPane borderJoueurs = new BorderPane();
+		AnchorPane aPlateau = new AnchorPane(); 
 		
+		///////////////////////////////////////////
 		Button bPause1 = new Button(); 
 		bPause1.setPrefSize(lhBoutonPause, lhBoutonPause);
 		bPause1.setMinSize(lhBoutonPause, lhBoutonPause);
@@ -57,8 +78,13 @@ public class PlateauPane extends BorderPane {
 		nomJoueur1.setFont(Font.font("Segoe UI", 20));
 		nomJoueur1.setTextFill(Color.BLACK);
 		
-		hBas.setMargin(j1, new Insets(0,500,0,118));
+		hBas.setMargin(j1, insetJGauche);
 		j1.setAlignment(Pos.CENTER);
+		j1.setBackground(new Background(new BackgroundFill(Color.LIGHTGREY, CornerRadii.EMPTY, null)));
+		j1.setMargin(nbPerso1, margeTexteJoueur);
+		j1.setMargin(nbCartes1, margeTexteJoueur);
+		j1.setMargin(nomJoueur1, margeTexteJoueur);
+		j1.setMinSize(tailleVBoxJoueur, tailleVBoxJoueur);
 		j1.getChildren().addAll(nbPerso1,nbCartes1,nomJoueur1);
 		
 		VBox j2 = new VBox();
@@ -68,12 +94,17 @@ public class PlateauPane extends BorderPane {
 		Label nbCartes2 = new Label("## de cartes l.70");
 		nbCartes2.setFont(Font.font("Segoe UI", 20));
 		nbCartes2.setTextFill(Color.BLACK);
-		Label nomJoueur2 = new Label("Nom Joueur 1 l.73");
+		Label nomJoueur2 = new Label("Nom Joueur 2 l.73");
 		nomJoueur2.setFont(Font.font("Segoe UI", 20));
 		nomJoueur2.setTextFill(Color.BLACK);
 		
-		hBas.setMargin(j2, new Insets(0,118,0,500));
+		hBas.setMargin(j2, insetJDroit);
 		j2.setAlignment(Pos.CENTER);
+		j2.setBackground(new Background(new BackgroundFill(Color.LIGHTGREY, CornerRadii.EMPTY, null)));
+		j2.setMargin(nbPerso2, margeTexteJoueur);
+		j2.setMargin(nbCartes2, margeTexteJoueur);
+		j2.setMargin(nomJoueur2, margeTexteJoueur);
+		j2.setMinSize(tailleVBoxJoueur, tailleVBoxJoueur);
 		j2.getChildren().addAll(nbPerso2,nbCartes2,nomJoueur2);
 		
 		
@@ -103,12 +134,17 @@ public class PlateauPane extends BorderPane {
 		Label nbCartes3 = new Label("## de cartes l.105");
 		nbCartes3.setFont(Font.font("Segoe UI", 20));
 		nbCartes3.setTextFill(Color.BLACK);
-		Label nomJoueur3 = new Label("Nom Joueur 1 l.108");
+		Label nomJoueur3 = new Label("Nom Joueur 3 l.108");
 		nomJoueur3.setFont(Font.font("Segoe UI", 20));
 		nomJoueur3.setTextFill(Color.BLACK);
 		
-		hHaut.setMargin(j3, new Insets(0,500,0,118));
+		hHaut.setMargin(j3, insetJGauche);
 		j3.setAlignment(Pos.CENTER);
+		j3.setBackground(new Background(new BackgroundFill(Color.LIGHTGREY, CornerRadii.EMPTY, null)));
+		j3.setMargin(nbPerso3, margeTexteJoueur);
+		j3.setMargin(nbCartes3, margeTexteJoueur);
+		j3.setMargin(nomJoueur3, margeTexteJoueur);
+		j3.setMinSize(tailleVBoxJoueur, tailleVBoxJoueur);
 		j3.getChildren().addAll(nbPerso3,nbCartes3,nomJoueur3);
 		
 		VBox j4 = new VBox();
@@ -118,12 +154,17 @@ public class PlateauPane extends BorderPane {
 		Label nbCartes4 = new Label("## de cartes l.120");
 		nbCartes4.setFont(Font.font("Segoe UI", 20));
 		nbCartes4.setTextFill(Color.BLACK);
-		Label nomJoueur4 = new Label("Nom Joueur 1 l.123");
+		Label nomJoueur4 = new Label("Nom Joueur 4 l.123");
 		nomJoueur4.setFont(Font.font("Segoe UI", 20));
 		nomJoueur4.setTextFill(Color.BLACK);
 		
-		hHaut.setMargin(j4, new Insets(0,118,0,500));
+		hHaut.setMargin(j4, insetJDroit);
 		j4.setAlignment(Pos.CENTER);
+		j4.setBackground(new Background(new BackgroundFill(Color.LIGHTGREY, CornerRadii.EMPTY, null)));
+		j4.setMargin(nbPerso4, margeTexteJoueur);
+		j4.setMargin(nbCartes4, margeTexteJoueur);
+		j4.setMargin(nomJoueur4, margeTexteJoueur);
+		j4.setMinSize(tailleVBoxJoueur, tailleVBoxJoueur);
 		j4.getChildren().addAll(nbPerso4,nbCartes4,nomJoueur4);
 		
 		
@@ -140,21 +181,30 @@ public class PlateauPane extends BorderPane {
 		Label nbCartes5 = new Label("## de cartes l.140");
 		nbCartes5.setFont(Font.font("Segoe UI", 20));
 		nbCartes5.setTextFill(Color.BLACK);
-		Label nomJoueur5 = new Label("Nom Joueur 1 l.143");
+		Label nomJoueur5 = new Label("Nom Joueur 5 l.143");
 		nomJoueur5.setFont(Font.font("Segoe UI", 20));
 		nomJoueur5.setTextFill(Color.BLACK);
 		
 		j5.setAlignment(Pos.CENTER);
+		j5.setBackground(new Background(new BackgroundFill(Color.LIGHTGREY, CornerRadii.EMPTY, null)));
+		j5.setMargin(nbPerso5, margeTexteJoueur);
+		j5.setMargin(nbCartes5, margeTexteJoueur);
+		j5.setMargin(nomJoueur5, margeTexteJoueur);
+		j5.setMinSize(tailleVBoxJoueur, tailleVBoxJoueur);
+		j5.setPrefSize(tailleVBoxJoueur, tailleVBoxJoueur);
+		j5.setMaxSize(tailleVBoxJoueur, tailleVBoxJoueur);
 		j5.getChildren().addAll(nbPerso5,nbCartes5,nomJoueur5);
-		
+		j5.setRotate(270);
 		
 		hDroite.getChildren().addAll(j5);		
-		hDroite.setAlignment(Pos.BOTTOM_CENTER);
-		hDroite.setRotate(270);
+		hDroite.setAlignment(Pos.CENTER_LEFT);
+		
 		
 		
 		///////////////////////
-		/*VBox j6 = new VBox();
+		/*
+		// obsolete 
+		VBox j6 = new VBox();
 		Label nbPerso6 = new Label("## personnages l.157");
 		nbPerso6.setFont(Font.font("Segoe UI", 20));
 		nbPerso6.setTextFill(Color.BLACK);
@@ -166,6 +216,11 @@ public class PlateauPane extends BorderPane {
 		nomJoueur6.setTextFill(Color.BLACK);
 		
 		j6.setAlignment(Pos.CENTER);
+		j6.setBackground(new Background(new BackgroundFill(Color.LIGHTGREY, CornerRadii.EMPTY, null)));
+		j6.setMargin(nbPerso6, margeTexteJoueur);
+		j6.setMargin(nbCartes6, margeTexteJoueur);
+		j6.setMargin(nomJoueur6, margeTexteJoueur);
+		j6.setMinSize(tailleVBoxJoueur, tailleVBoxJoueur);
 		j6.getChildren().addAll(nbPerso6,nbCartes6,nomJoueur6);
 		
 		
@@ -173,12 +228,122 @@ public class PlateauPane extends BorderPane {
 		hGauche.setAlignment(Pos.BOTTOM_CENTER);
 		hGauche.setRotate(90);*/
 		
+		/////////////////////////////////////////////////////
 		
-		this.setTop(hHaut);
-		this.setBottom(hBas);
-		this.setLeft(hGauche);
-		this.setRight(hDroite);
-		this.setCenter(sCentre);
+		aPlateau.setMaxSize(taillePlateau, taillePlateau);
+		
+		Label estBarricade1 = new Label("Barricadé\n"+"Oui/Non l.235");
+		estBarricade1.setFont(fontInfo);
+		estBarricade1.setTextFill(Color.BLACK);
+		
+		Label titre1 = new Label("Toilettes l.239");
+		titre1.setFont(fontTitre);
+		titre1.setTextFill(Color.BLACK);
+		
+		Label estFerme1 = new Label("Fermé\n"+"Oui/Non l.243");
+		estFerme1.setFont(fontInfo);
+		estFerme1.setTextFill(Color.BLACK);
+		
+		Label force1 = new Label("Force\ntotale de\nl'équipe :\n"+"## l.247");
+		force1.setFont(fontInfo);
+		force1.setTextFill(Color.BLACK);
+		
+		GridPane joueursPresents1 = new GridPane(); 
+		joueursPresents1.setPrefSize(200, 200);
+		joueursPresents1.setBackground(new Background(new BackgroundFill(Color.DARKGREY, CornerRadii.EMPTY, null)));
+		//todo 
+		
+		Label nbZombies1 = new Label("## de\nzombies l.256");
+		nbZombies1.setFont(fontInfo);
+		nbZombies1.setTextFill(Color.BLACK);
+		
+		
+		GridPane g1 = new GridPane(); 
+		g1.add(estBarricade1, 0, 0);
+		g1.add(titre1, 1, 0);
+		g1.add(estFerme1, 2, 0);
+		
+		g1.add(force1, 0, 1);
+		g1.add(joueursPresents1, 1, 1);
+		g1.add(nbZombies1, 2, 1);
+		
+		g1.setAlignment(Pos.CENTER);
+		g1.setBackground(new Background(new BackgroundFill(Color.LIGHTGREY, CornerRadii.EMPTY, null)));
+		
+		g1.setMargin(estBarricade1, margeLieu);
+		g1.setMargin(titre1, margeLieu);
+		g1.setMargin(estFerme1, margeLieu);
+		g1.setMargin(force1, margeLieu);
+		g1.setMargin(joueursPresents1, margeLieu);
+		g1.setMargin(nbZombies1, margeLieu);
+		
+		
+		aPlateau.setTopAnchor(g1, ((taillePlateau/2)-178.6/2));
+		aPlateau.setLeftAnchor(g1, ((taillePlateau/2)-421.33/2));
+		
+			
+		////
+		
+		Label estBarricade2 = new Label("Barricadé\n"+"Oui/Non l.286");
+		estBarricade2.setFont(fontInfo);
+		estBarricade2.setTextFill(Color.BLACK);
+		
+		Label titre2 = new Label("Cachou l.237");
+		titre2.setFont(fontTitre);
+		titre2.setTextFill(Color.BLACK);
+		
+		Label estFerme2 = new Label("Fermé\n"+"Oui/Non l.294");
+		estFerme2.setFont(fontInfo);
+		estFerme2.setTextFill(Color.BLACK);
+		
+		Label force2 = new Label("Force\ntotale de\nl'équipe :\n"+"## l.298");
+		force2.setFont(fontInfo);
+		force2.setTextFill(Color.BLACK);
+		
+		GridPane joueursPresents2 = new GridPane(); 
+		joueursPresents2.setPrefSize(200, 200);
+		joueursPresents2.setBackground(new Background(new BackgroundFill(Color.DARKGREY, CornerRadii.EMPTY, null)));
+		//todo 
+		
+		Label nbZombies2 = new Label("## de\nzombies l.307");
+		nbZombies2.setFont(fontInfo);
+		nbZombies2.setTextFill(Color.BLACK);
+		
+		
+		GridPane g2 = new GridPane(); 
+		g2.add(estBarricade2, 0, 0);
+		g2.add(titre2, 1, 0);
+		g2.add(estFerme2, 2, 0);
+		
+		g2.add(force2, 0, 2);
+		g2.add(joueursPresents2, 1, 2);
+		g2.add(nbZombies2, 2, 2);
+		
+		g2.setAlignment(Pos.CENTER);
+		g2.setBackground(new Background(new BackgroundFill(Color.LIGHTGREY, CornerRadii.EMPTY, null)));
+		
+		g2.setMargin(estBarricade2, margeLieu);
+		g2.setMargin(titre2, margeLieu);
+		g2.setMargin(estFerme2, margeLieu);
+		g2.setMargin(force2, margeLieu);
+		g2.setMargin(joueursPresents2, margeLieu);
+		g2.setMargin(nbZombies2, margeLieu);
+		
+		
+		aPlateau.setTopAnchor(g2, 10.0);
+		aPlateau.setLeftAnchor(g2, ((taillePlateau/2)-422.33/2));
+		
+		aPlateau.getChildren().addAll(g1,g2);
+		
+		aPlateau.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, null)));
+		
+		////////////////////////////////////////////////////
+		borderJoueurs.setTop(hHaut);
+		borderJoueurs.setBottom(hBas);
+		borderJoueurs.setLeft(hGauche);
+		borderJoueurs.setRight(hDroite);
+		
+		this.getChildren().addAll(borderJoueurs,aPlateau);
 		
 		sControl.registerNode(paneName, this);
 		sControl.setPaneOnTop(paneName);
