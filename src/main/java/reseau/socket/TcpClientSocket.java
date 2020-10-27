47860aa8d9e00c69d378da063e6725916cc31e6f
Sebastien Aglae <sebastien.aglae@gmail.com>
2020-10-27 19:55:09 +0100

    Support des binds client TCP

--- src/main/java/reseau/socket/TcpClientSocket.java
+++ src/main/java/reseau/socket/TcpClientSocket.java
@@ -97,4 +97,81 @@ public abstract class TcpClientSocket {
 
         return r;
     }
+
+    /**
+     * Se connecte au seveur et envoie un message
+     *
+     * @param ip      l'ip du serveur TCP serveur
+     * @param port    port du serveur TCP serveur
+     * @param message le message a envoyer
+     * @param ip1      l'ip du serveur TCP client
+     * @param port1    port du serveur TCP client
+     * @return reponse au paquet
+     */
+    public static String connect(String ip, int port, String message, String ip1, int port1) {
+        try {
+            host = InetAddress.getLocalHost();
+            logger.finest("Client on ip " + host.getHostAddress());
+        } catch (UnknownHostException e) {
+            logger.severe("Cannot get host : " + e.getMessage());
+        }
+
+        try {
+            return connect(InetAddress.getByName(ip), port, message, InetAddress.getByName(ip1), port1);
+        } catch (UnknownHostException e) {
+            logger.warning("Cannot find ip : " + e.getMessage());
+        }
+
+        return "";
+    }
+
+    /**
+     * Se connecte au seveur et envoie un message
+     * Il réessaye d'envoyer le paquer tout les 50ms
+     *
+     * @param inetAddress l'ip du serveur TCP
+     * @param port        port du serveur TCP
+     * @param message     le message a envoyer
+     * @param ip1      l'ip du serveur TCP client
+     * @param port1    port du serveur TCP client
+     * @return reponse au paquet
+     */
+    public static String connect(InetAddress inetAddress, int port, String message, InetAddress ip1, int port1) {
+        logger.finest("Client started !");
+
+        String r = "";
+        boolean error = false;
+        int retry = 0;
+        while (!error) {
+            try {
+                ObjectOutputStream outputStream;
+                ObjectInputStream inputStream;
+                Socket socket = new Socket(inetAddress, port, ip1, port1);
+                socket.setTcpNoDelay(false);
+                logger.log(Level.FINEST, "Client on port : {}", port);
+                outputStream = new ObjectOutputStream(socket.getOutputStream());
+                logger.log(Level.INFO, "Message sent : {0}", message);
+                outputStream.writeObject(message);
+                inputStream = new ObjectInputStream(socket.getInputStream());
+                r = (String) inputStream.readObject();
+                logger.log(Level.INFO, "Message received : {0}", r);
+                inputStream.close();
+                outputStream.close();
+                error = true;
+            } catch (IOException | ClassNotFoundException e) {
+                if (retry == 3)
+                    break;
+                logger.warning(MessageFormat.format("Cannot connect to the server : {0}", e.getMessage()));
+                logger.warning(MessageFormat.format("Retry # : {0}", retry));
+                retry++;
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException interruptedException) {
+                    interruptedException.printStackTrace();
+                }
+            }
+        }
+
+        return r;
+    }
 }
