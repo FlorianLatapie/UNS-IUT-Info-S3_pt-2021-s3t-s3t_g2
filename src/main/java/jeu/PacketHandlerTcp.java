4c715ecf0817347d7bf2e4bebeefbd2671b8bd50
Sebastien Aglae <sebastien.aglae@gmail.com>
2020-10-31 17:57:19 +0100

    Ajouts traitement TCP
    [TCP] AZLD, CDDCV, CDDJ

--- src/main/java/jeu/PacketHandlerTcp.java
+++ src/main/java/jeu/PacketHandlerTcp.java
@@ -7,7 +7,10 @@ import reseau.type.TypeJoueur;
 
 import java.net.Socket;
 import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.Random;
+
+import static java.lang.System.out;
 
 /**
  * <h1>Permet de gerer les packets</h1>
@@ -41,14 +44,56 @@ public class PacketHandlerTcp {
         switch (packet.getKey()) {
             case "DCP":
                 return dcp(packet, message, socket);
+            case "AZLD":
+                return azld(packet, message, socket);
+            case "CDDCV":
+                return cddcv(packet, message, socket);
+            case "CDDJ":
+                return cddj(packet, message, socket);
             default:
                 throw new IllegalStateException(
                         MessageFormat.format("[TCP] Il n''y a pas de traitement possible pour {0}", packet.getKey()));
         }
     }
 
-    private String dcp(Packet packet, String message, Socket socket) {
+    private String cddj(Packet packet, String message, Socket socket) {
+        core.getTempPaquet().add(message);
+        core.addCddj();
+        return "";
+    }
 
+    private String cddcv(Packet packet, String message, Socket socket) {
+        core.getTempPaquet().add(message);
+        core.setCddcv(true);
+        return "";
+    }
+
+    private String azld(Packet packet, String message, Socket socket) {
+        int z1 = new Random().nextInt(6) + 1;
+        int z2 = new Random().nextInt(6) + 1;
+        int z3 = new Random().nextInt(6) + 1;
+        int z4 = new Random().nextInt(6) + 1;
+        ArrayList<Integer> lieuZombie = new ArrayList<>();
+        lieuZombie.add(z1);
+        lieuZombie.add(z2);
+        lieuZombie.add(z3);
+        lieuZombie.add(z4);
+        core.setLieuZombie(lieuZombie);
+        //TODO AZLAZ lieuZombie
+        out.println(
+                core.getJeu().getChefVIgile() + " , chef des vigiles, regarde les résulats de l'arrivé des Zombies:");
+        for (Integer integer : lieuZombie) {
+            out.println(core.getJeu().getLieux().get(integer) + "-> Zombie + 1");
+        }
+        // l'afficher sur l'ecran du CV s'il y en a un et s'il a un perso sur le lieu 5
+        // (PC)
+        // regarder si un joueur utilise une carte camSecu et si oui l'afficher sur son
+        // ecran et defausse la carte
+        core.setAzld(true);
+        return nwm.getPacketsTcp().get("AZLAZ").build(lieuZombie, core.getPartieId(), core.getNumeroTour());
+    }
+
+    private String dcp(Packet packet, String message, Socket socket) {
         switch (core.getStatus()) {
             case ATTENTE:
                 System.out.println(socket.getLocalPort());
