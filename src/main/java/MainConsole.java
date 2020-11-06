416e18a79b66e94c8a5c79c6601e5eae2faa0f3f
Sebastien Aglae <sebastien.aglae@gmail.com>
2020-11-06 02:11:47 +0100

    IDJR avec reseau integré

--- src/main/java/MainConsole.java
+++ src/main/java/MainConsole.java
@@ -8,8 +8,6 @@ import reseau.tool.NetworkTool;
 public class MainConsole {
 	static Idjr idjr;
 	public static void main(String[] args) throws IOException {
-		idjr = new Idjr();
-		NetWorkManager nwm = new NetWorkManager(idjr);
-		nwm.initConnection(SideConnection.CLIENT, NetworkTool.getAliveLocalIp());
+		Idjr idjr = new Idjr(null);
 	}
 }
