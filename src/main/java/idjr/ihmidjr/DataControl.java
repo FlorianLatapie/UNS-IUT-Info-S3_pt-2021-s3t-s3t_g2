8265199b1b195b98d97daeec1b5be097dcc64fbf
Florian LATAPIE <70631622+FlorianLatapie@users.noreply.github.com>
2020-12-06 17:35:01 +0100

    ajout des images pour les cartes qui seront affichées dans l'idjr

--- src/main/java/idjr/ihmidjr/DataControl.java
+++ src/main/java/idjr/ihmidjr/DataControl.java
@@ -18,6 +18,7 @@ public class DataControl {
 	public final static Locale localeEN = new Locale("en", "UK");
 	public final static Locale localeFR = new Locale("fr", "FR");
 	public final static String localMsgURL = "Ressources.Textes.messages";
+	
 	public static final String FOND = "idjr/ihmidjr/images/fond.png";
 	public static final String PLATEAU = "idjr/ihmidjr/images/plateau.png";
 	public static final String ICONE = "idjr/ihmidjr/images/desktop Icon.jpg";
@@ -27,6 +28,23 @@ public class DataControl {
 	public static final String ROUGE = "idjr/ihmidjr/images/rouge.png";
 	public static final String BLEU = "idjr/ihmidjr/images/bleu.png";
 	public static final String JAUNE = "idjr/ihmidjr/images/jaune.png";
+
+	//Cartes
+	public static final String CARTE_BATTE = "idjr/ihmidjr/images/Cartes/Cartes_Armes/batte.png";
+	public static final String CARTE_CS = "idjr/ihmidjr/images/Cartes/Cartes_Armes/Canon Scié.png";
+	public static final String CARTE_GRENADE = "idjr/ihmidjr/images/Cartes/Cartes_Armes/grenade.png";
+	public static final String CARTE_HACHE = "idjr/ihmidjr/images/Cartes/Cartes_Armes/hache.png";
+	public static final String CARTE_REVOLVER = "idjr/ihmidjr/images/Cartes/Cartes_Armes/revolver.png";
+	public static final String CARTE_TRONCENNEUSE = "idjr/ihmidjr/images/Cartes/Cartes_Armes/tronconneuse.png";
+	
+	//Autres artes
+	public static final String CARTE_CACHETTE = "idjr/ihmidjr/images/Cartes/Cartes_Autres/cachette.png";
+	public static final String CARTE_CAMERA = "idjr/ihmidjr/images/Cartes/Cartes_Autres/camera.png";
+	public static final String CARTE_MATERIEL = "idjr/ihmidjr/images/Cartes/Cartes_Autres/matériel.png";
+	public static final String CARTE_MENACE = "idjr/ihmidjr/images/Cartes/Cartes_Autres/menace.png";
+	public static final String CARTE_SPRINT = "idjr/ihmidjr/images/Cartes/Cartes_Autres/sprint.png";
+	
+	
 	public static final ObservableList<Integer> nombreJoueur = FXCollections.observableArrayList(3, 4, 5, 6);
 	public static final ObservableList<Integer> nombreBot = FXCollections.observableArrayList(0, 1, 2, 3, 4, 5, 6);
 	public static final ObservableList<String> couleursJoueur = FXCollections.observableArrayList("Noir", "Vert",
