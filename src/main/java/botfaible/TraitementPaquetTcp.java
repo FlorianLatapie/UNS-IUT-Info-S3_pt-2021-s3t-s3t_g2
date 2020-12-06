f15c39beb3d4c54009a299f3b846313e32c7fac0
AlexDevauchelle <70631774+AlexDevauchelle@users.noreply.github.com>
2020-12-06 22:37:21 +0100

    Ajour case"CDZVDI" dans TPTcp

--- src/main/java/botfaible/TraitementPaquetTcp.java
+++ src/main/java/botfaible/TraitementPaquetTcp.java
@@ -126,6 +126,7 @@ public class TraitementPaquetTcp extends TraitementPaquet<Socket> {
 			recupCarte(packet, message);
 			break;
 
+		case "CDZVDI":
 		case "AZUCS":
 		case "PIPZ":
 		case "PFC":
