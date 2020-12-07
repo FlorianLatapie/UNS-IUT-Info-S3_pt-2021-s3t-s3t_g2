dd05f6e7a97de170db90759a99f009812e43b614
AlexDevauchelle <70631774+AlexDevauchelle@users.noreply.github.com>
2020-12-07 18:44:06 +0100

    Traitement case"RAZPA" dans BotMoyen

--- src/main/java/botmoyen/BotMoyen.java
+++ src/main/java/botmoyen/BotMoyen.java
@@ -394,6 +394,19 @@ public class BotMoyen {
 		}
 		
 	}
+
+	public void recupInfoPerso(Integer value, List<Object> value2,Integer lieu) {
+		List<Couleur> lc = new ArrayList<Couleur>();
+		List<Integer> li = new ArrayList<Integer>();
+		for (int i =0;i<value2.size();i=i+2) {
+			lc.add((Couleur)value2.get(i));
+			li.add((Integer)value2.get(i+1));
+		}
+		for (int i = 0 ; i<lc.size();i++) {
+			partie.deplacePerso(lc.get(i), li.get(i), lieu);
+		}
+		
+	}
 	
 	
 	
