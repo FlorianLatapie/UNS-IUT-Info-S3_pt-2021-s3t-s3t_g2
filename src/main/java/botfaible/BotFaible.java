7fd3dff9f64874c321e4a04c9227c1445f6e6a52
Sébastien Aglaé <38996444+sebastienaglae@users.noreply.github.com>
2020-12-10 02:02:02 +0100

    Heuristique yield

--- src/main/java/botfaible/BotFaible.java
+++ src/main/java/botfaible/BotFaible.java
@@ -48,12 +48,7 @@ public class BotFaible {
 		initReseau();
 
 		while (!isEstFini()) {
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			Thread.yield();
 		}
 		arreter();
 	}
