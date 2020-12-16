fbd4952436fa23f8ce2cf60d7070da3d79ad0324
AurélienARNAULT <aurelien.hand06@gmail.com>
2020-12-16 17:44:55 +0100

    Création controleurVote

--- src/main/java/pp/controleur/ControleurVote.java
+++ src/main/java/pp/controleur/ControleurVote.java
@@ -2,33 +2,121 @@ package pp.controleur;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+
+import com.sun.tools.sjavac.comp.dependencies.PublicApiCollector;
 
 import pp.Joueur;
 import pp.Lieu;
 import pp.Partie;
+import pp.reseau.VoteReseau;
+import reseau.socket.ControleurReseau;
 import reseau.type.Couleur;
+import reseau.type.VoteEtape;
 import reseau.type.VoteType;
 
 public class ControleurVote {
+	private VoteReseau vr;
 
-	public List<Object> Tour1(Partie jeu, Lieu l, VoteType tv){
-		List<Object> lo = new ArrayList<>();
-		List<Couleur> listeJoueursPresent = new ArrayList<>();
-		List<Integer> listeNbVoix = new ArrayList<>();
-		for (Joueur j : jeu.getJoueurSurLieu(l)) {
-			listeJoueursPresent.add(j.getCouleur());
-		}
-		for (Joueur j : jeu.getJoueurSurLieu(l)) {
-			listeNbVoix.add(j.getNbVoix(l, 1, 0));
-		}
-		List<Couleur> listeJoueursVotant = listeJoueursPresent;
-		System.out.println("Joueur Présent:" + listeJoueursPresent.size());
-		lo.add(listeJoueursPresent);
-		lo.add(listeJoueursVotant);
-		lo.add(listeNbVoix);
-		return lo;
+	public ControleurVote() {
+		vr = new VoteReseau();
 	}
-	
+
+	public Joueur phaseVote(Partie jeu, Lieu l, VoteType tv, String partieId, int numeroTour) {
+		Joueur joueur = phaseVoteTour(jeu, l, tv, VoteEtape.PRE, partieId, numeroTour);
+		if (joueur != null)
+			return joueur;
+		joueur = phaseVoteTour(jeu, l, tv, VoteEtape.SEC, partieId, numeroTour);
+		if (joueur != null)
+			return joueur;
+		if (tv == VoteType.MPZ)
+			return jeu.getJoueurSurLieu(l).get(new Random().nextInt(jeu.getJoueurSurLieu(l).size()));
+		return null;
+	}
+
+	public Joueur phaseVoteTour(Partie jeu, Lieu l, VoteType tv, VoteEtape ve, String partieId, int numeroTour) {
+		List<Couleur> joueursVotant = infoVote(jeu, l, tv, ve, partieId, numeroTour);
+		List<Integer> nbVoix = new ArrayList<>();
+		List<Couleur> votes = new ArrayList<>();
+		List<Integer> voixRecu = initVoixRecu(ve, jeu, l);
+		vr.demanderCarte(jeu, l, partieId, numeroTour);
+		traitementMenace(ve, jeu, nbVoix, l, partieId, numeroTour);
+		vr.demanderVote(jeu, l, partieId, numeroTour);
+		int i = 0;
+		for (Joueur j : jeu.getJoueurSurLieu(l)) {
+			Couleur couleurVote = vr.recupVote(j);
+			votes.add(couleurVote);
+			int a = 0;
+			for (Joueur jou : jeu.getJoueurSurLieu(l)) {
+				if (jou.getCouleur() == couleurVote)
+					voixRecu.set(a, voixRecu.get(a) + nbVoix.get(i));
+				a++;
+			}
+			i++;
+		}
+		vr.cloreVote(jeu, l, partieId, numeroTour);
+		Couleur couleurVote = eluVote(voixRecu, joueursVotant);
+		vr.informerResultatVote(jeu, couleurVote, voixRecu, votes, joueursVotant, partieId, numeroTour);
+		if (couleurVote == Couleur.NUL)
+			return null;
+		return jeu.getJoueurCouleur(couleurVote);
+	}
+
+	public List<Couleur> infoVote(Partie jeu, Lieu l, VoteType tv, VoteEtape ve, String partieId, int numeroTour) {
+		List<Couleur> joueursPresent = new ArrayList<>();
+		List<Integer> nbVoix = new ArrayList<>();
+		List<Couleur> joueursVotant = new ArrayList<>();
+		for (Joueur j : jeu.getJoueurSurLieu(l))
+			joueursPresent.add(j.getCouleur());
+		if (ve == VoteEtape.PRE) {
+			for (Joueur j : jeu.getJoueurSurLieu(l))
+				nbVoix.add(j.getNbVoix(l, 1, 0));
+			joueursVotant = joueursPresent;
+			vr.debutVote(jeu, tv, VoteEtape.PRE, joueursPresent, joueursVotant, nbVoix, partieId, numeroTour);
+		} else if (ve == VoteEtape.SEC) {
+			for (Joueur j : jeu.getJoueurs().values())
+				nbVoix.add(j.getNbVoix(l, 2, 0));
+			for (Joueur j : jeu.getJoueurs().values())
+				joueursVotant.add(j.getCouleur());
+			vr.debutVote(jeu, tv, VoteEtape.SEC, joueursPresent, joueursVotant, nbVoix, partieId, numeroTour);
+		}
+		return joueursVotant;
+	}
+
+	public List<Integer> initVoixRecu(VoteEtape ve, Partie jeu, Lieu l) {
+		List<Integer> voixRecu = new ArrayList<>();
+		if (ve == VoteEtape.PRE)
+			while (voixRecu.size() < jeu.getJoueurSurLieu(l).size())
+				voixRecu.add(0);
+		else if (ve == VoteEtape.SEC)
+			while (voixRecu.size() < jeu.getJoueurs().size())
+				voixRecu.add(0);
+		return voixRecu;
+	}
+
+	public void traitementMenace(VoteEtape ve, Partie jeu, List<Integer> nbVoix, Lieu l, String partieId,
+			int numeroTour) {
+		for (Joueur j : jeu.getJoueurSurLieu(l)) {
+			if (ve == VoteEtape.PRE) {
+				int nbMenace = vr.carteJoue(j);
+				nbVoix.add(j.getNbVoix(l, 1, nbMenace));
+				for (Joueur jou : jeu.getJoueurs().values())
+					vr.informeCarteJouee(l, jou, nbMenace, partieId, numeroTour);
+			} else if (ve == VoteEtape.SEC) {
+				int nbMenace = vr.carteJoue(j);
+				int a = 0;
+				for (Integer i : jeu.getJoueurs().keySet())
+					if (jeu.getJoueurs().get(i) == j)
+						a = i;
+				nbVoix.remove(a);
+				nbVoix.add(a, j.getNbVoix(l, 2, nbMenace));
+				for (Joueur jou : jeu.getJoueurs().values())
+					jou.getConnection().envoyer(ControleurReseau.construirePaquetTcp("PVIC", j.getCouleur(), nbMenace,
+							j.getNbVoix(l, 2, nbMenace), partieId, numeroTour));
+			}
+		}
+	}
+
 	public Couleur eluVote(List<Integer> listeVoixRecu, List<Couleur> listeJoueursPresent) {
 		int i = 0;
 		int nbVoteTemp = 0;
@@ -44,7 +132,5 @@ public class ControleurVote {
 		}
 		return couleurVote;
 	}
-	
-	
-	
+
 }
