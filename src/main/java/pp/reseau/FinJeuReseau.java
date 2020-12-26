add8ca25b987011caa67de7153a3f34b9408b483
AurélienARNAULT <aurelien.hand06@gmail.com>
2020-12-26 11:59:48 +0100

    Correction warning

--- src/main/java/pp/reseau/FinJeuReseau.java
+++ src/main/java/pp/reseau/FinJeuReseau.java
@@ -6,17 +6,17 @@ import reseau.socket.ControleurReseau;
 import reseau.type.CondType;
 
 /**
- * <h1>La classe FinJeuReseau </h1>. A pour rôle de traiter les paquets réseaux d'une fin de partie
+ * <h1>La classe FinJeuReseau</h1>. A pour rôle de traiter les paquets réseaux
+ * d'une fin de partie
  *
  * @author Aurelien
  * @version 1
  * @since 12/12/2020
  */
 public class FinJeuReseau {
-	
+
 	public void indiqueFinPartie(Partie jeu, Joueur vainqueur, CondType cond, String partieId, int numeroTour) {
-		String message = ControleurReseau.construirePaquetTcp("FP", cond, vainqueur.getCouleur(), partieId,
-				numeroTour);
+		String message = ControleurReseau.construirePaquetTcp("FP", cond, vainqueur.getCouleur(), partieId, numeroTour);
 		for (Joueur j : jeu.getJoueurs().values())
 			j.getConnection().envoyer(message);
 	}
